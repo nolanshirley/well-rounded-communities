19cd1046e098f95fe241e3001b56972527dbc0a7
Noland Shirley <noland.shirley@nwpharma.com>
2022-06-05 13:33:42 -0400

    firebase configurations

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,25 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+//   // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAyoKfQFZr1xwIuohMmZXegG4yCqanc0wE",
+//   authDomain: "well-rounded-communities.firebaseapp.com",
+//   projectId: "well-rounded-communities",
+//   storageBucket: "well-rounded-communities.appspot.com",
+//   messagingSenderId: "464681115134",
+//   appId: "1:464681115134:web:363648907255548a9c34af",
+//   measurementId: "G-WE8E6L662R"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
